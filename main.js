09a8b9a27db0d779404a38c033a67befbd18eab3
FuhuaChen <874166712@qq.com>
2018-04-21 19:03:19 +0800

    整理代码

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
-let n = 1
-let images = $('.images>img')
-$(`.images>img:nth-child(${n})`).addClass('current').siblings().addClass('enter')
+let n
+init()
 setInterval(()=>{
-    $(`.images>img:nth-child(${x(n)})`).addClass('leave').removeClass('current')
-    .one('transitionend',(e)=>{
-    $(e.currentTarget).removeClass('leave').addClass('enter')}
+    makeLeave(getImages(n)).one('transitionend',(e)=>{makeEnter($(e.currentTarget))}
 )
-$(`.images>img:nth-child(${x(n+1)})`).addClass('current').removeClass('enter')
+makeCurrent(getImages(n+1))
 n+=1
 },1500)
 
 
+
+
+
+
+
+
+
+
+
+function makeLeave(node){
+    return node.addClass('leave').removeClass('current')
+}
+function makeEnter(node){
+    return node.removeClass('leave').addClass('enter')
+}
+function makeCurrent(node) {
+    return node.addClass('current').removeClass('enter')
+}
+function getImages(n){
+    return $(`.images>img:nth-child(${x(n)})`)
+}
+function init(){
+    n = 1
+    $(`.images>img:nth-child(${n})`).addClass('current').siblings().addClass('enter')
+}
 function x(n){
+    let images = $('.images>img')
     if(n>images.length){
         n = n % (images.length)
         if(n===0){
