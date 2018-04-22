3b312b43c37ad55ddaf353c5a33d72d9380ceda1
FuhuaChen <874166712@qq.com>
2018-04-22 20:58:29 +0800

    修复切换页面后产生的bug

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let n
 init()
-setInterval(()=>{
-    makeLeave(getImages(n)).one('transitionend',(e)=>{makeEnter($(e.currentTarget))}
-)
-makeCurrent(getImages(n+1))
-n+=1
-},1500)
+let timer = autoPlay()
+
+document.addEventListener('visibilitychange', () => {
+    if (document.hidden) {
+        clearInterval(timer)
+    } else {
+        timer = autoPlay()
+    }
+})
 
 
 
@@ -16,29 +19,41 @@ n+=1
 
 
 
+function autoPlay() {
+    return setInterval(() => {
+        makeLeave(getImages(n)).one('transitionend', (e) => {makeEnter($(e.currentTarget))})
+        makeCurrent(getImages(n + 1))
+        n += 1
+    }, 1500)
+}
 
-function makeLeave(node){
+function makeLeave(node) {
     return node.addClass('leave').removeClass('current')
 }
-function makeEnter(node){
+
+function makeEnter(node) {
     return node.removeClass('leave').addClass('enter')
 }
+
 function makeCurrent(node) {
     return node.addClass('current').removeClass('enter')
 }
-function getImages(n){
+
+function getImages(n) {
     return $(`.images>img:nth-child(${x(n)})`)
 }
-function init(){
+
+function init() {
     n = 1
     $(`.images>img:nth-child(${n})`).addClass('current').siblings().addClass('enter')
 }
-function x(n){
+
+function x(n) {
     let images = $('.images>img')
-    if(n>images.length){
+    if (n > images.length) {
         n = n % (images.length)
-        if(n===0){
-            n=images.length
+        if (n === 0) {
+            n = images.length
         }
     }
     return n
